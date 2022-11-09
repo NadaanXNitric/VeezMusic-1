86def1e5c60d649539c2676697bd2cb7cec1889f
Ŋʌdʌŋ Pʌʀɩŋdʌ♡ <47463180+NadaanXNitric@users.noreply.github.com>
2022-11-09 07:52:44 +0530

    Update __init__.py

--- callsmusic/__init__.py
+++ callsmusic/__init__.py
@@ -1,2 +1,7 @@
+from pyrogram import Client
+from config import SESSION_NAME, API_ID, API_HASH
 from .callsmusic import pytgcalls, run
 from . import queues
+
+client = Client(SESSION_NAME, API_ID, API_HASH)
+run = client.run
